778e291c64ecb3521afabcddeb6b82228afab112
Pratham Choudhari <prathamchoudhari29@gmail.com>
2025-12-20 12:29:28 +0530

    Stored Document lengths for BM25 scoring

--- engine/index.py
+++ engine/index.py
@@ -6,10 +6,12 @@ class InvertedIndex:
     def __init__(self):
         self.index = defaultdict(dict)
         self.document_count = 0
+        self.doc_lengths = {}
 
     def add_document(self, doc_id: int, text: str):
         tokens = tokenize(text)
         term_frequencies = {}
+        self.doc_lengths[doc_id] = len(tokens)
 
         for token in tokens:
             term_frequencies[token] = term_frequencies.get(token, 0) + 1

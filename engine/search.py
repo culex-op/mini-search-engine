efdff45c6ce5a7ec0024449664d163d50f69b3f6
Pratham Choudhari <prathamchoudhari29@gmail.com>
2025-12-20 12:00:47 +0530

    reviewed the code

--- engine/search.py
+++ engine/search.py
@@ -1,21 +1,21 @@
 from engine.tokenizer import tokenize
 from engine.index import InvertedIndex
 from engine.scoring import TFIDFScorer
-
+from engine.tokenizer import tokenize
+from engine.index import InvertedIndex
+from engine.scoring import TFIDFScorer
 
 class SearchEngine:
     def __init__(self, index: InvertedIndex):
         self.index = index
         self.scorer = TFIDFScorer(index)
-
-    def search(self, query: str, top_k: int = 10) -> list[tuple[int, float]]:
+    def search(self , query : str , top_k : int = 10) -> list[tuple[int , float]]:
         query_terms = tokenize(query)
         scores = self.scorer.score(query_terms)
 
         ranked_results = sorted(
             scores.items(),
-            key=lambda x: x[1],
-            reverse=True
+            key = lambda x : x[1],
+            reverse = True 
         )
-
         return ranked_results[:top_k]

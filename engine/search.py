1c59c3d42725fa29c071c11ed0cf383f8745be63
Pratham Choudhari <prathamchoudhari29@gmail.com>
2025-12-21 17:54:06 +0530

    Switch search ranking from TF-IDF to BM25

--- engine/search.py
+++ engine/search.py
@@ -1,21 +1,21 @@
 from engine.tokenizer import tokenize
 from engine.index import InvertedIndex
-from engine.scoring import TFIDFScorer
-from engine.tokenizer import tokenize
-from engine.index import InvertedIndex
-from engine.scoring import TFIDFScorer
+from engine.bm25 import BM25Scorer
+
 
 class SearchEngine:
     def __init__(self, index: InvertedIndex):
         self.index = index
-        self.scorer = TFIDFScorer(index)
-    def search(self , query : str , top_k : int = 10) -> list[tuple[int , float]]:
+        self.scorer = BM25Scorer(index)
+
+    def search(self, query: str, top_k: int = 10) -> list[tuple[int, float]]:
         query_terms = tokenize(query)
         scores = self.scorer.score(query_terms)
 
         ranked_results = sorted(
             scores.items(),
-            key = lambda x : x[1],
-            reverse = True 
+            key=lambda x: x[1],
+            reverse=True
         )
+
         return ranked_results[:top_k]

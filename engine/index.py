f533c256d71300298991e193c088251bfe80da26
Pratham Choudhari <prathamchoudhari29@gmail.com>
2025-12-24 21:11:38 +0530

    Add boolean AND/OR query parsing support

--- engine/index.py
+++ engine/index.py
@@ -1,6 +1,26 @@
+from collections import defaultdict
+from engine.tokenizer import tokenize
 import json
 
-def save(self, filepath: str):
+class InvertedIndex:
+    def __init__(self):
+        self.index = defaultdict(dict)
+        self.document_count = 0
+        self.doc_lengths = {}
+        
+    def add_document(self, doc_id: int, text: str):
+        tokens = tokenize(text)
+        term_frequencies = {}
+        self.doc_lengths[doc_id] = len(tokens)
+
+        for token in tokens:
+            term_frequencies[token] = term_frequencies.get(token, 0) + 1
+
+        for term, freq in term_frequencies.items():
+            self.index[term][doc_id] = freq
+
+        self.document_count += 1
+    def save(self, filepath: str):
         data = {
             "index": self.index,
             "doc_lengths": self.doc_lengths,
@@ -9,7 +29,7 @@ def save(self, filepath: str):
         with open(filepath, "w") as f:
             json.dump(data, f)
 
-def load(self, filepath: str):
+    def load(self, filepath: str):
         with open(filepath, "r") as f:
             data = json.load(f)
 
--- engine/search.py
+++ engine/search.py
@@ -1,16 +1,23 @@
 from engine.tokenizer import tokenize
 from engine.index import InvertedIndex
 from engine.bm25 import BM25Scorer
+from engine.query_parser import QueryParser
 
 
 class SearchEngine:
     def __init__(self, index: InvertedIndex):
         self.index = index
         self.scorer = BM25Scorer(index)
+        self.parser = QueryParser()
 
     def search(self, query: str, top_k: int = 10) -> list[tuple[int, float]]:
-        query_terms = tokenize(query)
-        scores = self.scorer.score(query_terms)
+        terms, operator = self.parser.parse(query)
+
+        if operator == "OR":
+            scores = self.scorer.score(terms)
+
+        else:  # AND
+            scores = self._and_search(terms)
 
         ranked_results = sorted(
             scores.items(),
@@ -19,3 +26,16 @@ class SearchEngine:
         )
 
         return ranked_results[:top_k]
+
+    def _and_search(self, terms: list[str]) -> dict[int, float]:
+        doc_sets = []
+
+        for term in terms:
+            if term not in self.index.index:
+                return {}
+            doc_sets.append(set(self.index.index[term].keys()))
+
+        common_docs = set.intersection(*doc_sets)
+        scores = self.scorer.score(terms)
+
+        return {doc: score for doc, score in scores.items() if doc in common_docs}
